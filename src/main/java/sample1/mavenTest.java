ffbeb18e9438b1d7f9f8b70210897ddb25fb9a40
admin <admin@DESKTOP-HQBURN8>
2022-06-16 16:44:07 +0530

    # WARNING: head commit changed in the meantime
    lllf

---
+++ src/main/java/sample1/mavenTest.java
@@ -0,0 +1,11 @@
+package sample1;
+//hiiiihtyfguh
+public class mavenTest {
+	public static void main(String[] args)
+	{
+		System.out.println("Hiiiiiii");
+		System.out.println("Hiiiiiii");
+		System.out.println("Hiiiiiii");
+	}
+
+}
